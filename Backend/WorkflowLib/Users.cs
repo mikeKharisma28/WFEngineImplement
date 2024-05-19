6e664cf11f0614a01a9fc8575c83f77fb5252a01
Michael Laksa Kharisma <mike_charism@outlook.com>
2024-05-19 21:33:05 +0700

    Initial commit

---
+++ Backend/WorkflowLib/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowLib
+{
+    public static class Users
+    {
+        public static readonly List<User> Data = new()
+        {
+            new User {Name = "Peter", Roles = new List<string> {"User", "Manager"}},
+            new User {Name = "Margaret", Roles = new List<string> {"User"}},
+            new User {Name = "John", Roles = new List<string> {"Manager"}},
+            new User {Name = "Sam", Roles = new List<string> {"Manager"}},
+        };
+
+        public static readonly Dictionary<string, User> UserDict = Data.ToDictionary(u => u.Name);
+    }
+}
